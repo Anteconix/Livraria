390f72bdd9e8beb34db4d9e65610fef303e4087d
Anteconix <magnofilho2323@gmail.com>
2022-10-03 09:17:18 -0300

    19- Habilitando o Swagger e Redoc usando DRF Spectacular

--- livraria/settings.py
+++ livraria/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "core",
     "rest_framework_simplejwt",
     "corsheaders",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissions",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
@@ -114,3 +116,9 @@ MEDIA_ENDPOINT = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_files/")
 
 FILE_UPLOAD_PERMISSIONS = 0o640
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Livraria API",
+    "DESCRIPTION": "API para gerenciamento de livraria,, incluindo endpoints e documentação.",
+    "VERSION": "1.0.0",
+}
--- livraria/urls.py
+++ livraria/urls.py
@@ -6,9 +6,13 @@ from rest_framework.routers import DefaultRouter
 from core.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView,
-)
+    TokenRefreshView,)
 from media.router import router as media_router
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 
 router = DefaultRouter()
@@ -23,7 +27,18 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/media/", include(media_router.urls)),
-
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("api/", include(router.urls)),
 ]
 
 urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
